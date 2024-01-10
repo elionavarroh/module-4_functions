5d08bd60401a33663ad0b9b02e18834caca6e2c1
elionavarroh <elionavarroh@gmail.com>
2024-01-10 11:49:16 +0100

    change the comprobation of result inside the function

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,36 @@
 //VARIABLE
+
 let mivariable = 0;
 const botonNext = document.getElementById("Next");
 const botonBack = document.getElementById("Back");
 const botonReset = document.getElementById("Reset");
 const result = document.getElementById("result");
 
-
 //FUNCIONES
+
 function next () {
+    if (result !== null && result !== undefined && result instanceof HTMLHeadingElement) {
     mivariable = mivariable + 1;
-    result!.innerText = String(mivariable).padStart(2, "00");
+    result.innerText = String(mivariable).padStart(2, "00");
+}
 };
 
 function back () {
-    if (mivariable === 0) return;
-    mivariable = mivariable - 1;
-    result!.innerText = String(mivariable).padStart(2, "00");
+    if (result !== null && result !== undefined && result instanceof HTMLHeadingElement) {
+    if (mivariable === 0) {
+        console.log("El número es 0, por lo que no puede ir hacia atrás, porque sería negativo");
+    } else {
+        mivariable = mivariable - 1;
+    result.innerText = String(mivariable).padStart(2, "00");
+    }
+}
 };
 
 function reset () {
+    if (result !== null && result !== undefined && result instanceof HTMLHeadingElement) {
     mivariable = 0;
-    result!.innerText = String(mivariable).padStart(2, "00");
-};
-
-//RESULT
-
-if (result !== null && botonNext !== undefined && botonNext instanceof HTMLHeadingElement) {
-    botonNext.addEventListener("click", next)
-};
-
-if (result !== null && botonBack !== undefined && botonBack instanceof HTMLHeadingElement) {
-    botonBack.addEventListener("click", back)
-};
-
-if (result !== null && botonReset !== undefined && botonReset instanceof HTMLHeadingElement) {
-    botonReset.addEventListener("click", reset)
+    result.innerText = String(mivariable).padStart(2, "00");
+}
 };
 
 //EVENTS
